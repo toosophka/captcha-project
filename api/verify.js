999f5935dca36ae297e40f7dee1c86e6acf2abc7
Sophia Yakovleva <63459167+toosophka@users.noreply.github.com>
2026-04-08 20:38:17 +0700

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const token = req.body['smart-token'];
-  const secret = 'ysc2_hmmhq5eMblmzP4CQNdfoJVam3jIBTga2Wgzk8idc73fa2854';
+  const secret = 'ysc2_w4WERdThN7ic3OM7TCCnt0GuFcBZiWFVFWdiGfR5f5c69b36';
 
   if (!token) {
     return res.status(400).json({ success: false, message: 'Нет токена' });
